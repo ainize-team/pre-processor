e5fcc5d6585d1c059db927ac0e12778ba58b0955
LeeHoseop <ghtjq3909@naver.com>
2021-03-18 18:07:16 +0900

    Modify extension check

--- app.py
+++ app.py
@@ -324,7 +324,7 @@ def transform(file, options):
     os.remove(input_path)
     os.remove(result_path)
 
-    return io.BytesIO(data), filename, 200
+    return io.BytesIO(data), filename
 
 
 def option_transform(options):
@@ -363,8 +363,8 @@ def processor():
 
     result = req['output']
 
-    if len(result) == 3:
-        return send_file(result[0], mimetype='text/plain', attachment_filename=result[1]), result[2]
+    if result:
+        return send_file(result[0], mimetype='text/plain', attachment_filename=result[1]), 200
     else:
         return result
 
